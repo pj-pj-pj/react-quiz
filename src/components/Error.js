201b02387e9e155fbcf368bf607e3309ec1b2c4a
Paula Joyce Ucol <peaksenut@gmail.com>
2024-08-28 19:03:06 +0800

    change emoji on error message
    Co-authored-by: pj-01 pj00430@student.tsu.edu.ph

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,7 @@
 function Error() {
   return (
     <p className="error">
-      <span>💥</span> There was an error fecthing questions.
+      <span>⚠️</span> There was an error fecthing questions.
     </p>
   );
 }
